f5574ca639b1b6046f435f5c23898c64b0865ead
OnyangoT <teddybearochieng@gmail.com>
2019-08-06 08:29:15 +0300

    Added FirebaseUtil Class

--- app/src/main/java/com/ted/travelmantics/InsertActivity.java
+++ app/src/main/java/com/ted/travelmantics/InsertActivity.java
@@ -1,17 +1,51 @@
 package com.ted.travelmantics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class InsertActivity extends AppCompatActivity {
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+
+    EditText textTitle;
+    EditText textPrice;
+    EditText textDescription;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+        FirebaseUtil.openFbReference("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+        textTitle = (EditText) findViewById(R.id.text_title);
+        textPrice = (EditText) findViewById(R.id.text_price);
+        textDescription = (EditText) findViewById(R.id.text_description);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+             default:
+                 return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -19,5 +53,18 @@ public class InsertActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.save_menu, menu);
         return true;
+    }
+    public void saveDeal () {
+        String title = textTitle.getText().toString();
+        String price = textPrice.getText().toString();
+        String description = textDescription.getText().toString();
+        TravelDeal deal = new TravelDeal(title, price, description, "");
+        mDatabaseReference.push().setValue(deal);
+    }
+    public void clean () {
+        textTitle.setText("");
+        textPrice.setText("");
+        textDescription.setText("");
+        textTitle.requestFocus();
     }
 }
